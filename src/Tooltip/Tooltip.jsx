a698cc08db18f1525bbb7b5fda9bffe09a8ccb70
Javier Murillo <jmurillo93@gmail.com>
2021-10-11 02:27:02 +0200

    code improvements

--- src/Tooltip/Tooltip.jsx
+++ src/Tooltip/Tooltip.jsx
@@ -1,27 +1,62 @@
 import React, { useState } from "react";
-import "./Tooltip.css";
+import "./Tooltip.scss";
 
-const Tooltip = ({ delay, direction, content, children }) => {
-  let timeout;
-  const [show, setShow] = useState(true);
-
-  const showTooltip = () => {
-    setShow(true);
+/**
+ * @typedef {Object} Tooltip
+ * @property {string} id Html tooltip identifier
+ * @property {"top"|"right"|"bottom"|"left"} direction Position of the tooltip to be placed. Top by default
+ * @property {number} margin Margin in pixels from the arrow to the content. 16px by default
+ * @property {number} arrowSize Arrow size in pixels. 8px by default
+ * @property {JSX.Element} content Tooltip content
+ * @property {JSX.Element} children Children components wrapped by this
+ *
+ * @param {Tooltip} tooltipProps
+ * @returns {JSX.Element}
+ */
+const Tooltip = ({
+  id,
+  direction = "top",
+  margin = 16,
+  arrowSize = 8,
+  content,
+  children,
+}) => {
+  const [show, setShow] = useState(false);
+  const styles = {
+    BACKGROUND_COLOR: "#FFFFFF",
+    BORDER_COLOR: "#123412",
   };
-
-  const hideTooltip = () => {
-    clearInterval(timeout);
-    setShow(true);
+  /**
+   * Toggles the Tooltip
+   */
+  const toggleTooltip = () => {
+    setShow((prevShow) => !prevShow);
   };
 
   return (
     <div
       className="tooltip-container"
-      onMouseEnter={showTooltip}
-      onMouseLeave={hideTooltip}
+      onMouseEnter={toggleTooltip}
+      onMouseLeave={toggleTooltip}
+      onFocus={toggleTooltip}
+      onBlur={toggleTooltip}
     >
       {children}
-      {show && <div className={`tooltip ${direction || "top"}`}>{content}</div>}
+      {show && (
+        <div
+          id={id}
+          role="tooltip"
+          style={{
+            "--tooltip-background-color": styles.BACKGROUND_COLOR,
+            "--tooltip-border-color": styles.BORDER_COLOR,
+            "--tooltip-arrow-size": `${arrowSize}px`,
+            "--tooltip-margin": `${margin}px`,
+          }}
+          className={`tooltip ${direction || "top"}`}
+        >
+          {content}
+        </div>
+      )}
     </div>
   );
 };
